f365452427124ce4e069922537230a0aee2670d0
Allen Ussher <allen@ussher.ca>
2020-04-05 10:11:38 -0700

    Initial Commit

---
+++ FlickTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FlickTest
+//
+//  Created by Allen Ussher on 4/5/20.
+//  Copyright © 2020 Ussher Press. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
